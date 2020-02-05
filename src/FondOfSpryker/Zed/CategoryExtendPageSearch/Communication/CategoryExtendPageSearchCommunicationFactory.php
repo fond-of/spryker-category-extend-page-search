2ced33129053163d1640e46f2232d88e1be8175e
Julian Zimmermann <julian.zimmermann@fondof.de>
2020-02-05 09:45:45 +0100

    spryker update

--- src/FondOfSpryker/Zed/CategoryExtendPageSearch/Communication/CategoryExtendPageSearchCommunicationFactory.php
+++ src/FondOfSpryker/Zed/CategoryExtendPageSearch/Communication/CategoryExtendPageSearchCommunicationFactory.php
@@ -3,7 +3,6 @@
 namespace FondOfSpryker\Zed\CategoryExtendPageSearch\Communication;
 
 use FondOfSpryker\Zed\CategoryExtendPageSearch\CategoryExtendPageSearchDependencyProvider;
-use FondOfSpryker\Zed\CategoryExtendPageSearch\Communication\Plugin\PageMapExpander\CategoryKeyPageMapExpanderPlugin;
 use Spryker\Zed\CategoryPageSearch\Communication\CategoryPageSearchCommunicationFactory as SprykerCategoryPageSearchCommunicationFactory;
 
 class CategoryExtendPageSearchCommunicationFactory extends SprykerCategoryPageSearchCommunicationFactory
